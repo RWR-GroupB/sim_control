a0de6fc7326e81b520f2895dc229ee0415a8f925
Deepana Ishtaweera <dishtaweera@gmail.com>
2023-12-07 17:31:18 +0100

    add rl policy

---
+++ src/publish_rl_policy.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# python file to publish the policy output
+
+import rospy
+import numpy as np
+import os
+import sys
+from std_msgs.msg import Float32MultiArray
+
+class PublichRLPolicy:
+    
+    clamp_joints = np.float64([[-0.7, 0.7], [0, 0.83],[0, 0.96], #thumb
+                                [0, 0.65], [0, 0.742], #index
+                                [0, 0.65], [0, 0.742], #middle
+                                [0, 0.65], [0, 0.742]]) #pinky
+    
+    def __init__(self, topic='/hand/motors/cmd_joint_angles'):
+        self.ros_pub = rospy.Publisher(topic, Float32MultiArray, queue_size=10)
+        
+        self.joints_arr = np.zeros(9)
+        
+        # setting ros rate
+        self.rate = rospy.Rate(1)
+        
+    def publish_joints(self, policy_joints):
+        # publish the policy
+        while not rospy.is_shutdown():
+            for iter in range(policy_joints.shape[1]):
+                msg = Float32MultiArray()
+                self.joints_arr = policy_joints[1][iter]
+                
+                # clamp the joints
+                self.joints_arr = np.clip(self.joints_arr, self.clamp_joints[:,0], self.clamp_joints[:,1])
+                
+                # msg.data = self.joints_arr
+                # convert to degrees becore publishing
+                joints_arr_deg = np.rad2deg(self.joints_arr)
+                msg.data = joints_arr_deg
+                self.ros_pub.publish(msg)
+                self.rate.sleep()
+            break
+        
+
+if __name__ == '__main__':
+    
+    rospy.init_node('publish_rl_policy', anonymous = True)
+    
+    publish_rl_policy = PublichRLPolicy()
+    
+    # reap .npy file 
+    root = os.path.dirname(os.path.realpath(__file__))
+    policy_joints = np.load(root+'/../rl_recordings/rotation_direction_-1_dof_poses_test.npy')
+    
+    # print the size of the policy
+    rospy.loginfo('Policy size: %s', policy_joints.shape)
+    
+    publish_rl_policy.publish_joints(policy_joints)
